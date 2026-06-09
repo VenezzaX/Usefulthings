b40a38af6e8491fef2e641c45a83326981577942
VenezzaX <piozada243@gmail.com>
2026-06-09 00:46:52 -0300

    Create MyriadTest.lua

---
+++ MyriadTest.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGetAsync("https://github.com/Standard-2/Myriad/releases/latest/download/myriad.luau"))()
+
+-- For a specific version or beta:
+--[[
+local version = "v1.5.0-beta.1"
+loadstring(game:HttpGetAsync(`https://github.com/Proton-Utilities/Myriad/releases/download/{version}/myriad.luau`))()
+]]
